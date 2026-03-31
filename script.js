6b306d943f34aea6562e6731ab4475162743e05c
Roee Palmon <new.roeepalmon@gmail.com>
2026-03-31 10:26:41 -0700

    date display

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-//when i click on a button from the list it should blink the section its referencing
-const listItems = document.querySelectorAll('li');
-listItems.forEach(item => {
-    item.addEventListener('click', () => {
-        const sectionId = item;
-        const section = document.getElementById(sectionId);
-        console.log("Clicked on: " + section);
-        section.classList.add('blink');
-        setTimeout(() => {
-            section.classList.remove('blink');
-        }
-        , 1000); // Remove the blink class after 1 second
-    });
-});
+// Blink the referenced section when a navigation link is clicked
+const dateContainer = document.getElementById('date-container');
+const conversionContainer = document.getElementById('conversion-container');
+const numberContainer = document.getElementById('number-container');
 
+function getCurrentDate() {
+    const currentDate = new Date();
+    return currentDate.toString();
+}
+
+// Display the current date in the date container automatically updates every second
+function updateDate() {
+    //Today is MM/DD/YYYY
+    const currentDate = new Date();
+    const month = currentDate.getMonth() + 1; // Months are zero-based
+    const day = currentDate.getDate();
+    const year = currentDate.getFullYear();
+    const formattedDate = `Today is ${month}/${day}/${year}`;
+    dateContainer.textContent = formattedDate;
+    
+}
+
+updateDate(); // Initial call to display the date immediately
